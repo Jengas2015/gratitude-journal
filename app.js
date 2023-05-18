0762a289ff370e588ada00e13e0d051e2d7a1115
Jengas2015 <arejang@gmail.com>
2023-05-18 17:20:34 -0500

    added views and layouts folder and files.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const morgan = require("morgan")
+const exphbs = require('express-handlebars');
 const connectDB = require("./config/db")
+
 
 // Load config
 dotenv.config({path: './config/config.env'})
@@ -8,6 +11,16 @@ dotenv.config({path: './config/config.env'})
 connectDB()
 
 const app = express()
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: "main", extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 const PORT = process.env.PORT || 3000
 
