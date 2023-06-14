b6cc7e4536f0310d54c2aa291b6a48114c06cf18
Jengas2015 <arejang@gmail.com>
2023-06-14 16:01:33 -0500

    adds routing and views for edit button as well as helper function for pre-selecting public or private status. Also adds foundation for method override

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const methodOverride = require("method-override")
 const passport = require("passport")
 const session = require("express-session")
 const MongoStore = require("connect-mongo")
@@ -24,6 +25,16 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//Method Override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
@@ -31,7 +42,7 @@ if (process.env.NODE_ENV === "development") {
 
 //Handlebars Helpers
 
-const { formatDate,  truncate, stripTags, editIcon} = require("./helpers/hbs")
+const { formatDate,  truncate, stripTags, editIcon, select } = require("./helpers/hbs")
 
 // Handlebars
 app.engine('.hbs',
@@ -41,6 +52,7 @@ app.engine('.hbs',
       stripTags,
       truncate,
       editIcon,
+      select,
     },
     defaultLayout: "main",
     extname: '.hbs'
