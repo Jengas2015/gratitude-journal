5452b2d0fb426c6c8bafd6500d6edb19eb0f39d8
Jengas2015 <arejang@gmail.com>
2023-06-16 15:22:39 -0500

    adds delete functionality. Also added try catch methods for previous features.

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,6 @@
-const express = require ("express")
+const express = require("express")
 const router = express.Router()
-const {ensureAuth} = require("../middleware/auth")
+const { ensureAuth } = require("../middleware/auth")
 
 const Story = require("../models/Story")
 
@@ -30,10 +30,10 @@ router.post('/', ensureAuth, async (req, res) => {
 
 router.get('/', ensureAuth, async (req, res) => {
     try {
-        const stories = await Story.find({status: "public"})
-        .populate("user")
-        .sort({createdAt: "desc"})
-        .lean()
+        const stories = await Story.find({ status: "public" })
+            .populate("user")
+            .sort({ createdAt: "desc" })
+            .lean()
 
         res.render("stories/index", {
             stories,
@@ -47,44 +47,70 @@ router.get('/', ensureAuth, async (req, res) => {
 // @desc Show edit page
 // @route GET /stories/edit/:id
 
-router.get('/edit/:id', ensureAuth, async(req, res) => {
-    const story = await Story.findOne({
-        _id: req.params.id,
-    }).lean()
-
-    if (!story) {
-        res.render("error/404")
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id,
+        }).lean()
+    
+        if (!story) {
+            res.render("error/404")
+        }
+    
+        //checks to see if it's the owner of the story
+        if (story.user != req.user.id) {
+            res.redirect("/stories")
+        } else {
+            res.render("stories/edit", {
+                story,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render("error/500")
     }
 
-    //checks to see if it's the owner of the story
-    if (story.user != req.user.id) {
-        res.redirect("/stories")
-    } else {
-        res.render("stories/edit", {
-            story,
-        })
-    }
 })
 
 // @desc Update story
 // @route PUT /stories/:id
 
 router.put('/:id', ensureAuth, async (req, res) => {
-    let story = await Story.findById(req.params.id).lean()
+    try {
+        let story = await Story.findById(req.params.id).lean()
 
-    if (!story) {
-        return res.render("error/404")
+        if (!story) {
+            return res.render("error/404")
+        }
+        //checks to see if it's the owner of the story
+        if (story.user != req.user.id) {
+            res.redirect("/stories")
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+
+            res.redirect("/dashboard")
+        }
+
+    } catch (err) {
+        console.error(err)
+        return res.render("error/500")
     }
-    //checks to see if it's the owner of the story
-    if (story.user != req.user.id) {
-        res.redirect("/stories")
-    } else {
-        story = await Story.findOneAndUpdate({_id: req.params.id}, req.body, {
-            new: true,
-            runValidators: true
-        })
 
+})
+
+// @desc Delete story
+// @route DELETE /stories/:id
+
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.findOneAndDelete({ _id: req.params.id })
         res.redirect("/dashboard")
+    } catch (err) {
+        console.error(err)
+        return res.render("error/500")
     }
 
 })
